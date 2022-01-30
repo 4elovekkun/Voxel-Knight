963b5febf46974105cae83cb21ae3e15dc7fb6a4
4elovekkun <mixail.12042011@gmail.com>
2022-01-30 18:12:44 +0300

    Обновление Map Menu
    Полностью доделан UI в Map Menu

--- Voxel Knight/Assets/Scripts/MapController.cs
+++ Voxel Knight/Assets/Scripts/MapController.cs
@@ -8,35 +8,34 @@ public class MapController : MonoBehaviour
     public GameObject camera;
     public Transform cameraTransform;
     public float speed;
-    public float _rightLimit = 11.46f;
-    public float _leftLimit = -8.6f;
+    public float _rightLimit;
+    public float _leftLimit;
     public float _topLimit;
     public float _downLimit;
+    private Vector3 minLimitVector;
+    private Vector3 maxLimitVector;
 
     public void Start()
     {
         camera = GameObject.FindGameObjectWithTag("MainCamera");
         cameraTransform = camera.GetComponent<Transform>();
         speed = 0.5f;
+        minLimitVector = new Vector3(_leftLimit, _downLimit, cameraTransform.position.z);
+        maxLimitVector = new Vector3(_rightLimit, _topLimit, cameraTransform.position.z);
     }
 
     public void FixedUpdate()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            Debug.Log(Input.GetTouch(0).ToString());
+            cameraTransform.position = new Vector3
+                (
+                Mathf.Clamp(cameraTransform.position.x - touchDeltaPosition.x * speed * Time.deltaTime, _leftLimit - 0.5f, _rightLimit + 0.5f),
+                Mathf.Clamp(cameraTransform.position.y - touchDeltaPosition.y * speed * Time.deltaTime, _downLimit - 0.5f, _topLimit + 0.5f),
+                cameraTransform.position.z
+                );
             Debug.Log("Ñâàéï");
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                cameraTransform.position = new Vector3
-                    (
-                    Mathf.Clamp(cameraTransform.position.x - touchDeltaPosition.x * speed * Time.deltaTime, _leftLimit, _rightLimit),
-                    Mathf.Clamp(cameraTransform.position.y - touchDeltaPosition.y * speed * Time.deltaTime, _downLimit, _topLimit),
-                    cameraTransform.position.z
-                    );
-            }
         }
     }
 }
